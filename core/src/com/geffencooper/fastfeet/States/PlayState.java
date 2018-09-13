7969bf6448d79d6d4f3072d787bdeaeaf2d86a31
geffencooper <geffenc2789@gmail.com>
2018-09-13 03:14:53 -0700

    new

--- core/src/com/geffencooper/fastfeet/States/PlayState.java
+++ core/src/com/geffencooper/fastfeet/States/PlayState.java
@@ -8,11 +8,13 @@ import com.geffencooper.fastfeet.sprites.Runner;
 
 public class PlayState extends State {
     private Runner runner;
+    private Texture background;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         runner = new Runner((FastFeet.WIDTH / 2) - (50), (FastFeet.HEIGHT / 2)+ 50);
         cam.setToOrtho(false, FastFeet.WIDTH , FastFeet.HEIGHT);
+        background = new Texture("background.png");
     }
 
     @Override
@@ -34,6 +36,9 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(background, cam.position.x - (cam.viewportWidth / 2), -25);
+        //sb.draw(background, 0, 0, FastFeet.WIDTH, FastFeet.HEIGHT);
+
         sb.draw(runner.getRunner(), runner.getPosition().x, runner.getPosition().y);
         sb.end();
     }
